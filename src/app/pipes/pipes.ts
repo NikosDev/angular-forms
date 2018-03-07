a8f26fd602d7bce725339833d3c3c12abc3700ce
Nikos <nikk@hotmail.gr>
2018-03-07 23:16:06 +0200

    changed pipe

--- src/app/pipes/pipes.ts
+++ src/app/pipes/pipes.ts
@@ -8,8 +8,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
     transform(value: any, args?: any[]): any {
         return  value.filter(item => {
-            return args.length ? args.indexOf(item.name) != -1 : value;
+            const {name, code} = item;
+            const hasFound = args.indexOf(code) != -1 || args.indexOf(name) != -1;
+            return args.length ? hasFound : value;
+         // return args.length ? args.indexOf(item.name) != -1 : value;
         })
-    }
+      }
 }
 
